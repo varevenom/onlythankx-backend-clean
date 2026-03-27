0065e1ef7532cc960aea3a920496289b0d1ad9d5
NAVEEN <jnaveenc3@gmail.com>
2026-03-27 13:05:13 -0500

    add api route

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-app.get("/",(req,res)=> {  res.send(" My backend is LIVE!");
+app.get("/", (req, res) => {
+  res.send("My backend is LIVE!");
 });
 
-app.listen(PORT,() => {
- console.log(`Server running on port ${PORT}`);
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working 🚀" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
